f33b1779d1e9149aa0df63c3eee1b8349c026d43
LampJuice <dieez347@gmail.com>
2026-08-15 22:04:18 +0300

    edit: remove user initialization from app startup

--- app/src/main/java/com/lampjuice/budgetlight/ui/app/AppState.kt
+++ app/src/main/java/com/lampjuice/budgetlight/ui/app/AppState.kt
@@ -1,7 +1,5 @@
 package com.lampjuice.budgetlight.ui.app
 
 sealed interface AppState {
-    data object Loading : AppState
-
     data object Ready : AppState
 }
--- app/src/main/java/com/lampjuice/budgetlight/ui/app/AppViewModel.kt
+++ app/src/main/java/com/lampjuice/budgetlight/ui/app/AppViewModel.kt
@@ -1,39 +1,16 @@
 package com.lampjuice.budgetlight.ui.app
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.lampjuice.budgetlight.feature.auth.domain.usecase.InitializeUserUseCase
-import com.lampjuice.budgetlight.feature.budget.domain.usecase.SeedApplicationDataUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jakarta.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 
 @HiltViewModel
 class AppViewModel
 @Inject
-constructor(
-    private val initializeUserUseCase: InitializeUserUseCase,
-    private val seedApplicationDataUseCase: SeedApplicationDataUseCase,
-) : ViewModel() {
-    private val _state = MutableStateFlow<AppState>(AppState.Loading)
+constructor() : ViewModel() {
+    private val _state = MutableStateFlow<AppState>(AppState.Ready)
 
     val state = _state.asStateFlow()
-
-    init {
-        initialize()
-    }
-
-    private fun initialize() {
-        viewModelScope.launch {
-            val user = initializeUserUseCase()
-
-            seedApplicationDataUseCase(
-                userId = user.id,
-            )
-
-            _state.value = AppState.Ready
-        }
-    }
 }
