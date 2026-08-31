4d3575d9a8c399fd9ef6b2d58a4aa205b1076440
LampJuice <dieez347@gmail.com>
2026-08-31 22:19:45 +0300

    add: register user use case + tests

---
+++ backend/src/main/kotlin/com/lampjuice/feature/auth/domain/model/RegisterResult.kt
@@ -0,0 +1,9 @@
+package com.lampjuice.com.lampjuice.feature.auth.domain.model
+
+sealed interface RegisterResult {
+    data class Success(
+        val user: User
+    ) : RegisterResult
+
+    data object EmailAlreadyExists : RegisterResult
+}
